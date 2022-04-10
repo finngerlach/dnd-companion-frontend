af5ae15e2d9a70521ee1ccd20dc9a30bdcb8bb44
Finn Gerlach <72390121+finngerlach@users.noreply.github.com>
2022-04-10 14:46:59 +0200

    Change party component

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,6 +1,45 @@
+import { useState } from "react"
 import "../styles/App.scss"
-import Party from "./Party/Party"
+import Party, { PartyMember } from "./Party/Party"
+
+const party: PartyMember[] = [
+  {
+    name: "Detlef",
+    level: 1,
+  },
+  {
+    name: "Dieter",
+    level: 2,
+  },
+]
 
 export default function App() {
-  return <Party />
+  const [partyState, setPartyState] = useState(party)
+
+  function handlePartyStateAdd(item: PartyMember) {
+    console.log("blub in app")
+    const newState = [...partyState, item]
+    console.log(newState)
+    setPartyState(newState)
+    console.log(partyState)
+  }
+
+  function handlePartyStateItemChange(i: number, item: PartyMember) {
+    const newState = [...partyState]
+    newState[i] = item
+    setPartyState(newState)
+  }
+
+  function handlePartyStateRemove(i: number) {
+    setPartyState([...partyState].splice(i, 1))
+  }
+
+  return (
+    <Party
+      party={party}
+      onPartyStateAdd={handlePartyStateAdd}
+      onPartyStateItemChange={handlePartyStateItemChange}
+      onPartyStateRemove={handlePartyStateRemove}
+    ></Party>
+  )
 }
--- src/views/Party/Party.tsx
+++ src/views/Party/Party.tsx
@@ -1,11 +1,54 @@
+import { useCallback } from "react"
 import { Button } from "react-bootstrap"
-import PartyMember from "./PartyMember/PartyMember"
 
-export default function Party() {
+export type PartyMember = {
+  name?: string
+  level?: number
+  ac?: number
+  pp?: number
+}
+
+type PartyProps = {
+  party: PartyMember[]
+  onPartyStateAdd: (item: PartyMember) => void
+  onPartyStateItemChange: (i: number, item: PartyMember) => void
+  onPartyStateRemove: (i: number) => void
+}
+
+export default function Party({
+  party,
+  onPartyStateAdd,
+  onPartyStateItemChange,
+  onPartyStateRemove,
+}: PartyProps) {
+  const handlePartyStateAdd = () => {
+    console.log("blub")
+    onPartyStateAdd({})
+  }
+
+  const partyMemberRows = party.map((member: PartyMember, index: number) => (
+    <form className="row" key={index}>
+      <input
+        type="text"
+        placeholder="Name"
+        className="col"
+        value={member.name}
+      />
+      <input
+        type="text"
+        placeholder="Level"
+        className="col"
+        value={member.level}
+      />
+      <input type="text" placeholder="AC" className="col" value={member.ac} />
+      <input type="text" placeholder="PP" className="col" value={member.pp} />
+      <Button className="col">Remove</Button>
+    </form>
+  ))
   return (
     <>
-      <PartyMember></PartyMember>
-      <Button>Add</Button>
+      {partyMemberRows}
+      <Button onClick={handlePartyStateAdd}>Add</Button>
     </>
   )
 }
